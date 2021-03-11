fbe41d2597373f93f20a99d16c03b5311b723a9b
sharad <sharad13102185@gmail.com>
2021-03-11 12:45:56 +0530

    project set up

---
+++ src/test/java/com/sharad/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharad.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
