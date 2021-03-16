cacbb8306efc54f48cfe78998182e8937be7c20b
sharad <sharad13102185@gmail.com>
2021-03-17 00:29:47 +0530

    default profile

--- src/main/java/com/sharad/sfgdi/service/I18nEnGreetingServiceImpl.java
+++ src/main/java/com/sharad/sfgdi/service/I18nEnGreetingServiceImpl.java
@@ -3,7 +3,7 @@ package com.sharad.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nGreetingServiceImpl")
 public class I18nEnGreetingServiceImpl implements GreetingService {
     @Override
